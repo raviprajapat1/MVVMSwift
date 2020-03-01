40620aab1f16dc6ea31f73f3a4c219a28bf06745
Ravi <rprajapat.java@gmail.com>
2020-03-01 19:59:52 +0530

    project setup in Mvvm

---
+++ MVVMSwift/Utils/Extentions/UIColorExtention.swift
@@ -0,0 +1,12 @@
+//
+//  Extentions.swift
+//  Swift_MVVM_Boilerplate
+//
+//  Created by Ravi on 19/11/19.
+//  Copyright © 2019 Systango. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+}
