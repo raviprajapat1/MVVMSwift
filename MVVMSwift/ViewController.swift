d0880b99f28bece010e55e5bed81e8a46299d36a
Ravi <rprajapat.java@gmail.com>
2020-03-01 18:50:54 +0530

    Initial Commit

---
+++ MVVMSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMSwift
+//
+//  Created by Ravi Prajapat on 01/03/20.
+//  Copyright © 2020 Ravi Prajapat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVMSwiftTests/MVVMSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVMSwiftTests.swift
+//  MVVMSwiftTests
+//
+//  Created by Ravi Prajapat on 01/03/20.
+//  Copyright © 2020 Ravi Prajapat. All rights reserved.
+//
+
+import XCTest
+@testable import MVVMSwift
+
+class MVVMSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
